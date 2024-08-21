cc413b44e11c2b3cc97fb7fcc5f662df2989608f
Rajivrithika <rejiraj1986@gmail.com>
2024-08-21 12:37:19 +0530

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,10 @@
+import pyqrcode
+import streamlit as st
+s1="upi://pay?pa=rejiraj1986@oksbi&am="
+s2=st.text_input("Enter the amount")
+s3="&cu=INR"
+s=s1+s2+s3
+if st.button("Generate"):
+   url=pyqrcode.create(s)
+   url.png('myqr.png',scale=10)
+   st.image("myqr.png",caption="Generated QR")
